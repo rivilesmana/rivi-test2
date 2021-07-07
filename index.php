1e2bb11e48862e87b27d724eb678cbecac64e80f
rivilesmana <rivi.lesmana@gmail.com>
2021-07-07 11:05:31 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Coba Git Remote</title>
 </head>
 <body>
+<h1>Hello World!</h1>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, odio voluptatum praesentium fuga quasi error consequuntur quisquam blanditiis quis possimus dolores laborum qui, deleniti assumenda impedit, a sapiente magnam magni!</p>
     
 </body>
 </html>
